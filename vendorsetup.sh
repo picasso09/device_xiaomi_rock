67dc60378cd3a91ead38eb0cdb2be3def42bab81
picasso09 <119201284+picasso09@users.noreply.github.com>
2024-12-18 20:45:20 +0700

    rock: vendorsetup: Inherit dependencies
    Signed-off-by: picasso09 <119201284+picasso09@users.noreply.github.com>
    
    Date:      Wed Dec 18 20:45:20 2024 +0700
    Change-Id: 5afd7b81dbf5a4d11e3d4e2021a5245cc43c9838

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,20 @@
+echo "\033[1;33mCloning Dependencies\033[0m"
+
 if [ -d "hardware/xiaomi/megvii" ]; then
     rm -rf "hardware/xiaomi/megvii"
 fi
+
+# Kernel
+git clone https://github.com/picasso09/kernel_xiaomi_rock -b fifteen kernel/xiaomi/rock
+
+# Vendor
+git clone https://github.com/picasso09/proprietary_vendor_xiaomi_rock -b fifteen vendor/xiaomi/rock
+
+# Hardware
+git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.0 hardware/mediatek
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.0 hardware/xiaomi
+
+# Sepolicy
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+
+echo "\033[32mDone go cook\033[0m"
