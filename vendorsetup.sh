080260807d4833b828ee31e8baa94e3c9b766d0b
picasso09 <119201284+picasso09@users.noreply.github.com>
2025-01-16 15:41:59 +0700

    rock: vendorsetup: no clone signed key
    Signed-off-by: picasso09 <119201284+picasso09@users.noreply.github.com>
    
    Date:      Thu Jan 16 15:41:59 2025 +0700
    Change-Id: 25eca34c79a9b5fc2c1f759968d06735fe9d174c

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,6 +18,6 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.0 h
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
 
 # Signed
-git clone https://github.com/picasso09/vendor_afterlife-priv_keys vendor/afterlife-priv/keys
+#git clone https://github.com/picasso09/vendor_afterlife-priv_keys vendor/afterlife-priv/keys
 
 echo -e "\033[32mDone go cook\033[0m"
