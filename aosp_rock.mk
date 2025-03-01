ddddcb4d4811c310682908b47e048aee9f706994
picasso09 <picasso09DV@proton.me>
2025-03-02 05:38:01 +0700

    rock: branding to pe
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- aosp_rock.mk
+++ aosp_rock.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/rock/device.mk)
 
 # Inherit some common AfterLife stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Bootanimation Res
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -21,6 +21,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 USE_PIXEL_CHARGER := true
+CUSTOM_BUILD_TYPE := COMMUNITY
 
 DEVICE_MAINTAINER := picasso09
 DEVICE_MAINTAINER_LINK := t.me/picasso170606
@@ -31,7 +32,7 @@ TARGET_SUPPORTS_BLUR := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rock
-PRODUCT_NAME := arrow_rock
+PRODUCT_NAME := aosp_rock
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 22071219AI
 PRODUCT_MANUFACTURER := Xiaomi
