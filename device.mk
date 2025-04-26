79576bbfe3b7fe097db75847ed84113008679b28
picasso09 <picasso09DV@proton.me>
2025-04-27 01:39:47 +0700

    rock: drop fp xiaomi package
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- device.mk
+++ device.mk
@@ -120,10 +120,6 @@ PRODUCT_PACKAGES += \
     f2fs_io \
     check_f2fs
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint-service.xiaomi
-
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
