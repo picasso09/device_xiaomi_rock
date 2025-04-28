0cea37c431c3ba84b941afe42992491796d07294
picasso09 <picasso09DV@proton.me>
2025-04-28 16:17:35 +0700

    rock: fix typo name symlink camera
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,7 +9,6 @@ git clone https://github.com/picasso09/proprietary_vendor_xiaomi_rock -b fifteen
 # Hardware
 git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.2 hardware/mediatek
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
-rm -rf hardware/lineage/compat ; git clone https://github.com/picasso09/android_hardware_lineage_compat hardware/lineage/compat
 
 # Sepolicy
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
