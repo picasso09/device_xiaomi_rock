6fd55ab22c1a2b5d1dc3b00696551a82914ff6f6
picasso09 <picasso09DV@proton.me>
2025-05-01 14:21:12 +0700

    rock: add rock stone variant info
    Signed-off-by: picasso09 <picasso09DV@proton.me>
    
    Change-Id: f5499a2310fdefd5c037ecc2ea078b8ba1d368da

--- libinit/init_xiaomi_rock.cpp
+++ libinit/init_xiaomi_rock.cpp
@@ -9,6 +9,33 @@
 
 #include "vendor_init.h"
 
+static const variant_info_t rock_info = {
+    .hwc_value = "",
+    .sku_value = "rock",
+
+    .brand = "Redmi",
+    .device = "rock",
+    .marketname = "Redmi 11 Prime",
+    .model = "22071219AI",
+    .build_fingerprint = "Redmi/rock/rock:13/TP1A.220624.014/V14.0.6.0.TLUIDXM:user/release-keys"
+};
+
+static const variant_info_t stone_info = {
+    .hwc_value = "",
+    .sku_value = "stone",
+
+    .brand = "POCO",
+    .device = "stone",
+    .marketname = "POCO M5",
+    .model = "22071219CG",
+    .build_fingerprint = "POCO/stone_p_global/stone:13/TP1A.220624.014/V14.0.6.0.TLUIDXM:user/release-keys"
+};
+
+static const std::vector<variant_info_t> variants = {
+    rock_info,
+    stone_info,
+};
+
 void vendor_load_properties() {
     search_variant(variants);
 }
