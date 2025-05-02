559267faf583b954979af99df5f63e47e5519ca2
picasso09 <picasso09DV@proton.me>
2025-05-02 07:09:19 +0700

    rock: prop: disable force debugable
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- config/prop/default.mk
+++ config/prop/default.mk
@@ -9,7 +9,7 @@ endif
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.secure=0 \
     ro.adb.secure=0 \
-    ro.force.debuggable=1 \
+    ro.force.debuggable=0 \
 # Bluetooth
 # ro.bluetooth.a2dp_offload.supported=false
 # ro.vendor.mtk.bt_bleaudio_enable=false 
