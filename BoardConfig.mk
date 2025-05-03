88e7d95f47cb3e137dff7b37253e4bca4a28dc42
picasso09 <picasso09DV@proton.me>
2025-05-03 11:38:43 +0700

    rock: sepolicy: Label ro.fota.oem and ro.product.marketname
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -214,7 +214,7 @@ TARGET_USES_MKE2FS := true
 
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
-#SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
