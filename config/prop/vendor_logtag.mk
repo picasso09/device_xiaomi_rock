a8b319d85e4b3a3bf31a194ffd80ae91da5a9c40
xyzuniverse <dev.xyzuniverse@gmail.com>
2025-06-07 09:20:47 +0000

    rock: Suppress libperfmgr logspam
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- config/prop/vendor_logtag.mk
+++ config/prop/vendor_logtag.mk
@@ -225,5 +225,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.log.tag.hwcomposer=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.HWUI=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.FrameTracker=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.C2MtkBufferManager=$(VENDOR_LOG_LEVEL)
+    persist.log.tag.C2MtkBufferManager=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.libperfmgr=$(VENDOR_LOG_LEVEL)
 
