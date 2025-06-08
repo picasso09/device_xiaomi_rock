fcae9a25854a94db4692965b81522e03f2f2332a
picasso09 <picasso09DV@proton.me>
2025-06-08 12:24:21 +0700

    rock: clean tree
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,5 @@
 echo -e "\033[1;33mCloning Dependencies\033[0m"
 
-if [ -d "hardware/xiaomi/megvii" ]; then
-    rm -rf "hardware/xiaomi/megvii"
-fi
-
 # Kernel
 git clone https://github.com/picasso09/kernel_xiaomi_rock -b fifteen kernel/xiaomi/rock
 
@@ -16,8 +12,5 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 har
 
 # Sepolicy
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-20 device/mediatek/sepolicy_vndr
-
-# Signed
-#git clone https://github.com/picasso09/vendor_afterlife-priv_keys vendor/afterlife-priv/keys
 
 echo -e "\033[32mDone go cook\033[0m"
