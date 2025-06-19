83da45f960676a2c0d61e6f4bafcf67308306ca4
picasso09 <picasso09DV@proton.me>
2025-06-19 20:52:52 +0000

    rock: fix typo path libsource
    Signed-off-by: picasso09 <picasso09DV@proton.me>
    
    Date:      Thu Jun 19 20:52:52 2025 +0000
    Change-Id: a88b195dd96a77bb15ccb84ab9fc5a388013886b

--- extract-files.sh
+++ extract-files.sh
@@ -89,7 +89,7 @@ function blob_fixup() {
         vendor/lib64/hw/mt6789/android.hardware.camera.provider@2.6-impl-mediatek.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
-            system_ext/lib64/libsource.so)
+            system/lib64/libsource.so)
             "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
             ;;
             vendor/bin/hw/android.hardware.gnss-service.mediatek |\
