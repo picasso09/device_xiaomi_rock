b0df61b48ce9f728541d6176a2ebdf3bdc253acd
picasso09 <picasso09DV@proton.me>
2025-06-23 21:03:24 +0000

    rock: ab device no needed health recovery
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- device.mk
+++ device.mk
@@ -159,7 +159,6 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
 # Charger
