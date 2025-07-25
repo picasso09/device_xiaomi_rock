51e3598fd13a5f55b8efc3a00bdccd569a7abed4
picasao09 <picasso09DV@proton.me>
2025-07-26 00:58:05 +0700

    rock: use IR xiaomi service
    Signed-off-by: picasso09 <picasso09DV@proton.me>
    
    Date:      Sat Jul 26 00:58:05 2025 +0700
    Change-Id: 43b43a4b5e12d33aa44bd26df722a8aa53c479e6

--- device.mk
+++ device.mk
@@ -521,5 +521,5 @@ PRODUCT_PACKAGES += \
 # ConsumerIr
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service \
+    android.hardware.ir@1.0-service.xiaomi \
     android.hardware.ir-V1-ndk.vendor
