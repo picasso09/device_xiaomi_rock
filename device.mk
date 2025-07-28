9d0dee90960a3e56dcd58112e45a8bf459558d80
picasao09 <picasso09DV@proton.me>
2025-07-28 09:44:19 +0700

    rock: drop mtkper stub packages prebuilt already
    Signed-off-by: picasso09 <picasso09DV@proton.me>
    
    Date:      Mon Jul 28 09:44:19 2025 +0700
    Change-Id: e689d456ed8a0004a67fe797941f271ddeb9d1a1

--- device.mk
+++ device.mk
@@ -271,10 +271,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     android.hardware.power@1.3.vendor
 
-PRODUCT_PACKAGES += \
-    libmtkperf_client_vendor \
-    libmtkperf_client
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
