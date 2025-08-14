a9aa8a8fd4a6ba881b8a1dd097a1d66e5626c115
picasao09 <picasso09DV@proton.me>
2025-08-14 10:03:56 +0700

    rock: camera: import GcamGo
    Signed-off-by: picasso09 <picasso09DV@proton.me>
    
    Date:      Thu Aug 14 10:03:56 2025 +0700
    Change-Id: a708eacad37292d17c0d84eb0a07443840373173

--- device.mk
+++ device.mk
@@ -409,6 +409,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
 # Google camera
 PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
+# Google PDF
+PRODUCT_PACKAGES += \
     GooglePDFViewer
 
 # Power Off Alarm
