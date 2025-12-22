12ccd390e953aaa4f71b50d88706df929e1517fd
picasso09 <picasso09DV@proton.me>
2025-12-22 19:39:26 +0700

    rock: Clear LIBNATIVEWINDOW symbol versions for libneuralnetworks_sl_driver_mtk_prebuilt
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -46,6 +46,15 @@ blob_fixups: blob_fixups_user_type = {
     .regex_replace('on property:vts(.|\n)*', ''),
     ('vendor/lib64/libmtkcam_stdutils.so', 'vendor/lib64/hw/mt6789/android.hardware.camera.provider@2.6-impl-mediatek.so', 'vendor/lib64/hw/mt6789/vendor.mediatek.hardware.pq@2.15-impl.so'): blob_fixup()
     .replace_needed('libutils.so', 'libutils-v32.so'),
+     'vendor/lib64/mt6789/libneuralnetworks_sl_driver_mtk_prebuilt.so': blob_fixup()
+	.clear_symbol_version('AHardwareBuffer_allocate')
+	.clear_symbol_version('AHardwareBuffer_describe')
+	.clear_symbol_version('AHardwareBuffer_createFromHandle')
+	.clear_symbol_version('AHardwareBuffer_getNativeHandle')
+	.clear_symbol_version('AHardwareBuffer_lock')
+	.clear_symbol_version('AHardwareBuffer_lockPlanes')
+	.clear_symbol_version('AHardwareBuffer_release')
+	.clear_symbol_version('AHardwareBuffer_unlock'),
 
     # GNSS
     ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
