59d803c127df137a09547eec378f64989c25a129
picasso09 <picasso09DV@proton.me>
2025-12-22 19:41:09 +0700

    rock: patched Unresolved symbol: __android_log_print some lib
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -55,6 +55,8 @@ blob_fixups: blob_fixups_user_type = {
 	.clear_symbol_version('AHardwareBuffer_lockPlanes')
 	.clear_symbol_version('AHardwareBuffer_release')
 	.clear_symbol_version('AHardwareBuffer_unlock'),
+    ('vendor/lib64/libteei_daemon_vfs.so', 'vendor/lib64/mt6789/libaaa_ltm.so', 'vendor/lib64/mt6789/lib3a.flash.so', 'vendor/lib64/mt6789/lib3a.ae.stat.so', 'vendor/lib64/mt6789/lib3a.sensors.color.so', 'vendor/lib64/mt6789/lib3a.sensors.flicker.so', 'vendor/lib64/libSQLiteModule_VER_ALL.so'): blob_fixup()
+        .add_needed('liblog.so'),
 
     # GNSS
     ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
