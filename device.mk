37ec97f503b7f948798f098947452f50e7d3ae9b
picasso09 <picasso09DV@proton.me>
2025-12-30 01:09:20 +0700

    rock: add more soong namespace
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- device.mk
+++ device.mk
@@ -345,6 +345,8 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
 	$(LOCAL_PATH) \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/mediatek/libmtkperf_client \
 	hardware/google/interfaces \
 	hardware/google/pixel \
 	hardware/mediatek \
