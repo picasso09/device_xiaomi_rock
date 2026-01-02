05077743fb98132fab06fa88851306212a795279
picasao09 <picasso09DV@proton.me>
2026-01-02 18:27:08 +0700

    rock: preinit custom lineage compat
    Signed-off-by: RzaIX <75961923+RzaIX@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,6 +9,8 @@ git clone https://github.com/picasso09/proprietary_vendor_xiaomi_rock -b lineage
 # Hardware
 git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-23.0 hardware/mediatek
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
+rm -rf hardware/lineage/compat
+git clone https://github.com/picasso09/android_hardware_lineage_compat -b lineage-23.2 hardware/lineage/compat
 
 # Sepolicy
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.0 device/mediatek/sepolicy_vndr
