62e1eb2c6952c9c27f45b54269f75bbfbe15eaf7
picasso09 <picasso09DV@proton.me>
2026-01-03 13:58:47 +0000

    rock: fix missing symbol libsysenv error: Unresolved symbol: _ZN7android4base4TrimERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -85,7 +85,7 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libbase_shim.so'),
 
     # NVRAM
-    ('vendor/lib/libnvram.so', 'vendor/lib64/libnvram.so'): blob_fixup()
+    ('vendor/lib/libnvram.so', 'vendor/lib64/libnvram.so', 'vendor/lib/libsysenv.so', 'vendor/lib64/libsysenv.so'): blob_fixup()
 	.add_needed('libbase_shim.so'),
 
     # GNSS
