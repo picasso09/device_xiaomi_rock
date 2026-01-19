095c840536f1fd5e1a351e69ff53953da061d016
picasso09 <picasso09DV@proton.me>
2026-01-19 22:13:46 +0700

    rock: enable to build new gemini bootanimation
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- aosp_rock.mk
+++ aosp_rock.mk
@@ -16,6 +16,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Bootanimation Res
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_GEMINI := true
 
 # ARROW flags
 TARGET_SUPPORTS_QUICK_TAP := true
