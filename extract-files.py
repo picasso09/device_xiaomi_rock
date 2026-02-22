d70fb11fcda2fd06e3fbbb5edc0c0aac118b9e78
picasso09 <picasso09DV@proton.me>
2026-02-22 18:57:57 +0700

    rock: Patch audio with libtinyxml2-v34 from compat Google broke ABI for tinyXML2 in Android 16 QPR2, causing fatal crashes as per logcats.
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -83,6 +83,10 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/ese_spi_nxp.so': blob_fixup()
         .add_needed('libbase_shim.so'),
 
+    # AUDIO
+    ('vendor/lib/hw/audio.primary.mediatek.so', 'vendor/lib64/hw/audio.primary.mediatek.so') : blob_fixup()
+    .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+
     # NVRAM
     ('vendor/lib/libnvram.so', 'vendor/lib64/libnvram.so', 'vendor/lib/libsysenv.so', 'vendor/lib64/libsysenv.so'): blob_fixup()
 	.add_needed('libbase_shim.so'),
