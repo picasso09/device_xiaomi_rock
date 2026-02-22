68e3c48ac486dd23a5da0b0d5eaf5c5e1f3e0ac3
picasso09 <picasso09DV@proton.me>
2026-02-22 22:22:30 +0700

    rock: setup: set dependencies target branch into lineage-23.2
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,13 +7,13 @@ git clone https://github.com/picasso09/kernel_xiaomi_rock -b lineage-23.0 kernel
 git clone https://github.com/picasso09/proprietary_vendor_xiaomi_rock -b lineage-23.0 vendor/xiaomi/rock
 
 # Hardware
-git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-23.0 hardware/mediatek
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-23.2 hardware/mediatek
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.2 hardware/xiaomi
 rm -rf hardware/lineage/compat
 git clone https://github.com/picasso09/android_hardware_lineage_compat -b lineage-23.2 hardware/lineage/compat
 
 # Sepolicy
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.0 device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.2 device/mediatek/sepolicy_vndr
 
 # Common IMS
 git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen vendor/mediatek/ims
