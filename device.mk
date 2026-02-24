679790b3343aad04f8b957f690219ffba0bfdcfe
picasso09 <picasso09DV@proton.me>
2026-02-24 07:26:28 +0700

    rock: sensors: build dynamic sensor_hal
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- device.mk
+++ device.mk
@@ -162,6 +162,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
     android.hardware.sensors@2.0-subhal-impl-1.0 \
+    sensors.dynamic_sensor_hal \
     sensors.xiaomi.v2
 
 PRODUCT_COPY_FILES += \
