88ca7f1eec8a5d0f0140d0ab36a62a5a4193469e
picasso09 <picasso09DV@proton.me>
2026-02-24 21:28:06 +0700

    rock: silent logspam graphics composer
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- config/prop/vendor_logtag.mk
+++ config/prop/vendor_logtag.mk
@@ -226,5 +226,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.log.tag.HWUI=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.FrameTracker=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.C2MtkBufferManager=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.libperfmgr=$(VENDOR_LOG_LEVEL)
+    persist.log.tag.libperfmgr=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.android.hardware.graphics.composer@2.3-service=$(VENDOR_LOG_LEVEL)
 
