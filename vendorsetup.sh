45f45c9f04b71664c671832abe6f30ad96bb6870
picasso09 <picasso09DV@proton.me>
2026-02-24 21:34:02 +0700

    rock: setup: set target branch to lineage-23.2
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
 echo -e "\033[1;33mCloning Dependencies\033[0m"
 
 # Kernel
-git clone https://github.com/picasso09/kernel_xiaomi_rock -b lineage-23.0 kernel/xiaomi/rock
+git clone https://github.com/picasso09/kernel_xiaomi_rock -b lineage-23.2 kernel/xiaomi/rock
 
 # Vendor
-git clone https://github.com/picasso09/proprietary_vendor_xiaomi_rock -b lineage-23.0 vendor/xiaomi/rock
+git clone https://github.com/picasso09/proprietary_vendor_xiaomi_rock -b lineage-23.2 vendor/xiaomi/rock
 
 # Hardware
 git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-23.2 hardware/mediatek
