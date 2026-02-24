b4a886e93b9d7d32148b6e953a01e224848892ad
picasso09 <picasso09DV@proton.me>
2026-02-24 21:22:57 +0700

    rock: inherit signing keys if available
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- device.mk
+++ device.mk
@@ -28,6 +28,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
+# Keys
+$(call inherit-product-if-exists, vendor/lineage/signed/keys/keys.mk)
+
 # Call proprietary blob setup
 $(call inherit-product, vendor/xiaomi/rock/rock-vendor.mk)
 
