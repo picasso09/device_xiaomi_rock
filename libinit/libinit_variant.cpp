60f04f491750a030d6bd1e010838f67cb1e2ba15
picasso09 <picasso09DV@proton.me>
2026-02-27 22:41:01 +0700

    rock: redefined build identity force build description use stock
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -42,7 +42,5 @@ void set_variant_props(const variant_info_t variant) {
         property_override("bluetooth.device.default_name", variant.marketname, true);
         set_ro_build_prop("fingerprint", variant.build_fingerprint);
         property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint);
-
-        property_override("ro.build.description", fingerprint_to_description(variant.build_fingerprint));
     }
 }
--- lineage_rock.mk
+++ lineage_rock.mk
@@ -33,8 +33,9 @@ PRODUCT_MODEL := 22071219AI
 PRODUCT_MANUFACTURER := Xiaomi
 
 BUILD_HOSTNAME := picasso09
-BUILD_FINGERPRINT :=Redmi/rock/rock:14/UP1A.231005.007/V816.0.12.0.ULUMIXM:user/release-keys
-PRIVATE_BUILD_DESC="rock-user 14 UP1A.231005.007 V816.0.12.0.ULUMIXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="missi_phone_global_ext4-user 14 UP1A.231005.007 V816.0.12.0.ULUMIXM release-keys" \
+    BuildFingerprint=Redmi/rock/rock:14/UP1A.231005.007/V816.0.12.0.ULUMIXM:user/release-keys \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
