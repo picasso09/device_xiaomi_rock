e529592fcf7e3fa7adbf53d2aebf1c3377d3782b
picasso09 <picasso09DV@proton.me>
2026-02-28 19:51:00 +0700

    rock: set Dalvik to Default 4GB heap configuration * Our libinit is missing on GSIs and DSUs
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
