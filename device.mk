3454fbcb4f1d15470140f41a2fd1df4b1365402b
picasso09 <picasso09DV@proton.me>
2026-03-24 23:56:58 +0700

    rock: set Dalvik to Default 6GB heap configuration * 4G not enough for some app vm heap utility use root action so fallback to use 6G value
    Signed-off-by: picasso09 <picasso09DV@proton.me>
    
    Date:      Tue Mar 24 23:56:58 2026 +0700
    Change-Id: c408008b98a5912fa602c2591df8bfd097c4c073

--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@ DEVICE_PATH := device/xiaomi/rock
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
